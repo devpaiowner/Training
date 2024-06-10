a9d69f00a712ab2caadd1b051b29db4e275b0dd0
Pawan <pawan.k@ninehertzindia.com>
2024-06-10 14:16:20 +0530

    commit

--- React js/src/yup/Auth/EditProfileValidation.ts
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-import ValidationMessage from '../../constants/Validation/ValidationMessage';
-
-const EditProfileValidation={}
-
-export default EditProfileValidation;
-
