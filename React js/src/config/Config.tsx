40a97d9735dce1143aff6fd16a299d73d8698fce
devpaiowner <162875148+devpaiowner@users.noreply.github.com>
2024-06-10 14:10:31 +0530

    Update Config.tsx

--- React js/src/config/Config.tsx
+++ React js/src/config/Config.tsx
@@ -48,13 +48,6 @@ export const Config = {
     REACT_APP_API_BASE_URL: 'https://demo.dev9server.com/dais/api/',
     IS_USER_LOGIN_TOKEN: getFromStorage({ key: StorageConstants?.IS_USER_LOGIN_TOKEN }),
     ACTIVE_ROLE: getFromStorage({ key: 'role' }) || UserRoleTypes?.Rider,
-    GOOGLE_MAP_API_KEY: 'AIzaSyAXixh8KRY9rjm6Y6EPX48BqXXd8a9Rml4',
-    ENCODE_SECRET_KEY_1: '#&*BB&G*&#&*#G8754454^&*T*&GMAN&^#$$ISH883782&**78',
-    ENCODE_SECRET_KEY_2: '!@#$%^&*()UJCFFGS4548MAN/*+5+5as8as7d6t7ISH&^*^&^*',
-    // GOOGLE_CLIENT_ID: '548587511179-j4qcsn54gbsdp7p8eaeoornf0t53qp79.apps.googleusercontent.com',
-    GOOGLE_CLIENT_ID: '879972649909-2b2jge9j1r5pvsr5275hg24lc61ualvc.apps.googleusercontent.com', //MM
-    FACEBOOK_SOCIAL_LOGIN_APP_ID: '641088710888267',
-    FACEBOOK_SOCIAL_LOGIN_APP_SECRET: '3511c97b5116ce2cb5eac92b2d869421',
     USER_DETAILS: getFromStorage({ key: StorageConstants?.USER_DETAIL }),
     IMAGE_BASE_URL: 'https://demo.dev9server.com/dais',
 };
